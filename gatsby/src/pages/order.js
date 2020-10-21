01ff0dbe2ad49c827d5b306817b27394c88c1a77
rimatla <alt.coelho@gmail.com>
2020-10-20 22:03:36 -0400

    order form plus custom hooks and utils

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,11 +1,96 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
+import calculatePizzaPrice from '../utils/calculatePizzaPrice';
+import formatMoney from '../utils/formatMoney';
+import OrderStyles from '../styles/OrderStyles';
+import MenuItemStyles from '../styles/MenuItemStyles';
 
-export default function Order() {
+export default function Order({ data }) {
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+  });
+
+  const pizzas = data.pizzas.nodes;
   return (
     <>
       <SEO title="Order a Pizza" />
-      <p>Orders!</p>
+      <OrderStyles>
+        <fieldset>
+          <legend>Your Info</legend>
+          <label htmlFor="name">
+            Name
+            <input
+              id="name"
+              type="text"
+              name="name"
+              value={values.name}
+              onChange={updateValue}
+            />
+          </label>
+          <label htmlFor="email">
+            Email
+            <input
+              id="email"
+              type="email"
+              name="email"
+              value={values.email}
+              onChange={updateValue}
+            />
+          </label>
+        </fieldset>
+        <fieldset className="menu">
+          <legend>Menu</legend>
+          {pizzas.map((pizza) => (
+            <MenuItemStyles key={pizza.id}>
+              <Img
+                fluid={pizza.image.asset.fluid}
+                width="50"
+                height="50"
+                alt={pizza.name}
+              />
+              <div>
+                <h2>{pizza.name}</h2>
+              </div>
+              <div>
+                {['S', 'M', 'L'].map((size) => (
+                  <button type="button">
+                    {size} {formatMoney(calculatePizzaPrice(pizza.price, size))}
+                  </button>
+                ))}
+              </div>
+            </MenuItemStyles>
+          ))}
+        </fieldset>
+        <fieldset className="order">
+          <legend>Order</legend>
+        </fieldset>
+      </OrderStyles>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        price
+        image {
+          asset {
+            fluid(maxWidth: 100) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
---
+++ gatsby/src/utils/useForm.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+// custom hook
+export default function useForm(defaults) {
+  const [values, setValues] = useState(defaults);
+
+  function updateValue(e) {
+    // check if value is a number and parse
+    let { value } = e.target;
+    if (e.target.value === 'number') {
+      value = parseInt(e.target.value);
+    }
+    setValues({
+      // copy the existing values into it
+      ...values,
+      // update the new value that changed
+      [e.target.name]: value,
+    });
+  }
+
+  return { values, updateValue };
+}
